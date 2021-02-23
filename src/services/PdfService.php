80d43c3c93847cc8143898cb70b0217eb8e2fca5
Benjamin <boublou@hotmail.fr>
2021-02-23 14:05:37 +0100

    add some comment to pdf test

--- src/services/PdfService.php
+++ src/services/PdfService.php
@@ -13,16 +13,22 @@ class PdfService
     public function AbsenceAuth(): \FPDF
     {
         $pdf = new Fpdi();
-        //dd(__DIR__);
-        try {
-            $pageCount = $pdf->setSourceFile(__DIR__ . "/../../templates/pdf/AA.pdf");
-        } catch (\Exception $e) {
-            dd($e);
-        }
-        $pageId = $pdf->importPage(1);
 
+        /* Importation du model */
+        $pdf->setSourceFile(__DIR__ . "/../../templates/pdf/AA.pdf");
+        $pageId = $pdf->importPage(1);
         $pdf->AddPage();
         $pdf->useImportedPage($pageId, 10 ,10,200);
+
+        /* Test d'écriture */
+        $pdf->SetFont('Times');
+        $pdf->SetXY(30,30);
+        $pdf->Write(1, utf8_decode('Bonjour à toi'));
+
+        $pdf->SetXY(60, 40);
+        $pdf->Write(1, utf8_decode('je suis toujours là'));
+
+
         return $pdf;
     }
 }
